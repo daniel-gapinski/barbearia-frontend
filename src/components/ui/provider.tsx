4f6041b716806c07cd0ba84a4f3250a28554d1b0
danielgapinski <daniel.gapinski@hotmail.com>
2025-02-13 10:30:16 -0300

    Movendo página theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -5,7 +5,7 @@ import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode";
-import { system } from "@/pages/theme";
+import { system } from "../../../theme";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
